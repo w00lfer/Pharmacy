777fcf119cdefa14687cdf752b14dcad6f445572
Woolfer <arek.pazola1998@gmail.com>
2020-01-08 21:59:28 +0100

    Add maps and custom resolvers

---
+++ Pharmacy/Mappings/Resolvers/OrderCostResolver.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Pharmacy.Models;
+using Pharmacy.Services.Interfaces;
+using Pharmacy.ViewModels;
+
+namespace Pharmacy.Mappings.Resolvers
+{
+    public class OrderCostResolver : IValueResolver<OrderCreateViewModel, Order, double>
+    {
+        private readonly IMedicineService _medicineService;
+
+        public OrderCostResolver(IMedicineService medicineService) => _medicineService = medicineService;
+
+        public double Resolve(OrderCreateViewModel orderCreateViewModel, Order order, double destMember, ResolutionContext context) =>
+            _medicineService.GetMedicineByIdAsync(order.MedicineId).Result.Price * order.Amount;
+    }
+}
---
+++ Pharmacy/Mappings/Resolvers/WithPrescriptionResolver.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Pharmacy.Models;
+using Pharmacy.Services.Interfaces;
+using Pharmacy.ViewModels;
+
+namespace Pharmacy.Mappings.Resolvers
+{
+    public class WithPrescriptionResolver : IValueResolver<Order, OrderIndexViewModel, bool>
+    {
+        private readonly IMedicineService _medicineService;
+
+        public WithPrescriptionResolver(IMedicineService medicineService) => _medicineService = medicineService;
+
+        public bool Resolve(Order order, OrderIndexViewModel orderIndexViewModel, bool destMember, ResolutionContext context) =>
+            _medicineService.GetMedicineByIdAsync(order.MedicineId).Result.WithPrescription;
+    }
+}
